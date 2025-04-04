c5fe27fa1d975d0ad9664a232ba873a7e9cec5ac
PasanSasmika <pasansasmika198@gmail.com>
2025-04-04 16:37:00 +0530

    Create Desktop sidebar and show the currentUser , if user logedin👤

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
-import ModeToggle from "@/components/modeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
 
 export default function Home() {
   return (
    <div className="m-4">
-    
-    <SignedOut>
-              <SignInButton mode="modal">
-                <Button>
-                  Sign In
-                </Button>
-              </SignInButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            <Button variant={"secondary"}>
-              Click me
-            </Button>
-            <ModeToggle/>
+    <h1>Home Page Content</h1>
    </div>
   );
 }
